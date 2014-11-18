35dd262775b5269ea620a0b8b2ce2f304437ddd9
Ryan Thompson <service@aiwebsystems.com>
2014-11-18 12:29:00 -0600

    Donzo

--- src/BooleanFieldType.php
+++ src/BooleanFieldType.php
@@ -2,21 +2,58 @@
 
 use Anomaly\Streams\Platform\Addon\FieldType\FieldType;
 
+/**
+ * Class BooleanFieldType
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Addon\FieldType\Boolean
+ */
 class BooleanFieldType extends FieldType
 {
+
+    /**
+     * The database column type.
+     *
+     * @var string
+     */
     public $columnType = 'boolean';
 
-    public function input()
-    {
-        $options = array(
-            'id' => $this->fieldName(),
-        );
+    /**
+     * The input view.
+     *
+     * @var string
+     */
+    protected $inputView = 'field_type.boolean::input';
 
-        return \Form::checkbox($this->fieldName(), true, $this->value, $options);
-    }
+    /**
+     * The input wrapper view.
+     *
+     * @var string
+     */
+    protected $wrapperView = 'field_type.boolean::wrapper';
 
-    protected function onSet($value)
+    /**
+     * Get view data for the input.
+     *
+     * @return array
+     */
+    public function getInputData()
     {
-        return boolean($value);
+        $data = parent::getInputData();
+
+        $data['checked'] = ($this->getValue());
+
+        // This is API configuration for the plugin.
+        $data['on']  = trans($this->getConfig('on', 'misc.enabled'));
+        $data['off'] = trans($this->getConfig('off', 'misc.disabled'));
+
+        $data['size'] = $this->getConfig('size', 'normal');
+
+        $data['onstyle']  = $this->getConfig('onstyle', 'success');
+        $data['offstyle'] = $this->getConfig('offstyle', 'default');
+
+        return $data;
     }
 }
