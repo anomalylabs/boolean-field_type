847d69a576a9631a61e887ac4fbe6d0161b437a0
MarceauKa <marceau@casals.fr>
2015-07-15 18:23:52 +0200

    Ajout de la traduction française

---
+++ resources/lang/fr/config.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'on_text'       => [
+        'label'        => 'Texte pour "On"',
+        'instructions' => 'Le texte utilisé pour l\'état "On"',
+        'placeholder'  => 'On'
+    ],
+    'on_color'      => [
+        'label'        => 'Couleur "On"',
+        'instructions' => 'La couleur utilisée pour l\'état "On"',
+        'green'        => 'Vert',
+        'blue'         => 'Bleu',
+        'orange'       => 'Orange',
+        'red'          => 'Rouge',
+        'gray'         => 'Gris'
+    ],
+    'off_text'      => [
+        'label'        => 'Texte "Off"',
+        'instructions' => 'Le texte utilisé pour l\'état "Off"',
+        'placeholder'  => 'Off'
+    ],
+    'off_color'     => [
+        'label'        => 'Couleur "Off"',
+        'instructions' => 'La couleur utilisée pour l\'état "Off"',
+        'green'        => 'Vert',
+        'blue'         => 'Bleu',
+        'orange'       => 'Orange',
+        'red'          => 'Rouge',
+        'gray'         => 'Gris'
+    ],
+    'default_value' => [
+        'label'        => 'Valeur par défaut',
+        'instructions' => 'Quel est l\'état par défaut de l\'interrupteur ?'
+    ]
+];
