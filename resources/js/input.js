1a0e1df95f93363b315e8321a8f30499fa93373d
Ryan Thompson <ryan@pyrocms.com>
2017-01-13 02:06:47 -0600

    Initialize once

--- resources/js/input.js
+++ resources/js/input.js
@@ -3,9 +3,12 @@ $(document).on('ajaxComplete ready', function () {
     setTimeout(function () {
 
         // Initialize form switches
-        $('[data-provides="anomaly.field_type.boolean"]').each(function () {
-            $(this).bootstrapSwitch();
+        $('[data-provides="anomaly.field_type.boolean"]:not([data-initialized])').each(function () {
+
+            $(this)
+                .attr('data-initialized', '')
+                .bootstrapSwitch();
         });
-        
+
     }, 10);
 });
